dfed07e47136a1d4870acfeafd6606225687ba93
Jacob Ross <jross.thumbsup@gmail.com>
2017-04-01 01:39:03 -0700

    added some comments and cleaned up

--- src/components/Orders.vue
+++ src/components/Orders.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="orders-container">
+
+    <!-- Orders Form -->
     <div class="create-order-container container">
       <div class="row bg-accent create-order-group">
         <div class="col-sm-8">
+
+          <!-- Order Name -->
           <div class="form-group">
             <input type="text" class="form-control" id="orderName" placeholder="Order Name" v-model="name" v-focus>
           </div>
+
         </div>
         <div class="col-sm-4">
-         <div class="form-group">
+
+          <!-- Create, Save and Cancel Buttons -->
+          <div class="form-group">
             <div>
               <button class="btn btn-info full-width" @click="createEditOrderClickHandler">{{createEditText}}</button>
             </div>
@@ -16,10 +23,15 @@
               <button class="btn btn-warning full-width" @click="cancelEditOrderClickHandler">Cancel</button>
             </div>
           </div>
+
         </div>
       </div>
     </div>
+
+    <!-- Orders Table -->
     <div class="order-table-container container bg-info">
+
+      <!-- Heading Row -->
       <div class="mod-row row hidden-xs bg-accent">
         <div class="col-sm-4">
           <h4>Name</h4>
@@ -31,6 +43,8 @@
           <h4>Actions</h4>
         </div>
       </div>
+
+      <!-- Order Row -->
       <div class="order-row mod-row row" :class="{ 'bg-row-accent': index % 2 !== 0 }" v-for="(order, index) in orders">
         <div class="col-sm-4 vcenter-sm">
           <p class="bold-sm">{{order.orderName}}</p>
@@ -43,23 +57,21 @@
           <button class="btn btn-danger" @click="deleteOrderClickHandler(order.orderId)">Delete</button>
         </div>
       </div>
+
     </div>
+    
   </div>
 </template>
 
 <script>
-import Vue from 'vue'
+// Get vuex helper functions
 import {mapGetters, mapActions} from 'vuex'
-
-Vue.directive('focus', {
-    inserted: function (el) {
-        el.focus()
-    }
-})
 
 export default {
   name: 'orders',
   data () {
+    // Data properties local to this component
+    // and can be referenced by using the 'this' context.
     return {
       name:'',
       createEditText:'Create Order',
@@ -71,44 +83,56 @@ export default {
     focus: {
       // When the bound element is inserted into the DOM...
       inserted(el) {
-        // Focus the element
+        // Focus the element with the v-focus attribute
         el.focus()
       },
+      // After the containing component has updated...
       update(el) {
+        // Focus the element with the v-focus attribute
         el.focus()
       }
     }
   },
   created(){
+    // When this component is created, hit the
+    // API and request orders from the database
+    // to store them in the vuex state.
   	this.retrieveOrders()
   },
   computed:{
+    // Computed property that references
+    // the orders stored in the state.
   	...mapGetters(['orders'])
   },
   methods:{
+    // Various vuex actions defined by us
+    // to alter orders in DynamoDB (CRUD). 
   	...mapActions(['retrieveOrders', 'createOrder', 'deleteOrder', 'editOrder']),
+    // Create/Save button handler
     createEditOrderClickHandler(){
       if(this.name){
         if(this.editMode){
+          // Submit edit
           this.editOrder({id: this.id, body:{name: this.name}})
-          this.cancelEditOrderClickHandler()
         }
         else{
+          // Submit new order
           this.createOrder({body: {name: this.name}})
         }
+        // Clear input field
+        this.cancelEditOrderClickHandler()
       }
       else{
         alert('Name field required')
       }
-      
-      
     },
+    // Delete button handler
     deleteOrderClickHandler(id){
       if(confirm('Are you sure you want to delete this order?')){
         this.deleteOrder({id})
       }
     },
-
+    // Edit button handler
     editOrderClickHandler(id){
       const index = this.orders.findIndex((order) =>{
         return order.orderId === id
@@ -119,7 +143,7 @@ export default {
       this.editMode = true
       this.id = id
     },
-
+    // Cancel button handler
     cancelEditOrderClickHandler(){
       this.editMode = false
       this.createEditText = "Create Order"
